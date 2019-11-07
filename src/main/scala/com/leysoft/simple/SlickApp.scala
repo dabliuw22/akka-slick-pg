805d379aa3bda7c08272fb904f60d5b5a671d3db
Will Leyton <wleyton@addi.com>
2019-11-06 19:30:01 -0500

    Add score field

--- src/main/scala/com/leysoft/simple/SlickApp.scala
+++ src/main/scala/com/leysoft/simple/SlickApp.scala
@@ -8,6 +8,8 @@ import akka.stream.scaladsl.{Sink, Source}
 import slick.basic.DatabaseConfig
 import slick.jdbc.{GetResult, JdbcProfile}
 
+import scala.util.Random
+
 object SlickApp extends App {
   implicit val system = ActorSystem("slick-pg")
   implicit val materializer = ActorMaterializer()
@@ -19,11 +21,14 @@ object SlickApp extends App {
   userRepository.findAll
     .runWith(Sink.foreach { user => system.log.info(s"findAll: $user") })
 
-  userRepository.save(User(name = "username23"))
+  userRepository.save(User(name = s"username${Random.between(10, 100)}", score = Random.between(0, 100)))
     .runWith(Sink.foreach { count => system.log.info(s"save: $count") })
 
-  userRepository.findByName("username23")
+  userRepository.findByName("username5")
     .runWith(Sink.foreach { user => system.log.info(s"findByName: $user") })
+
+  userRepository.deleteById(5)
+    .runWith(Sink.foreach { count => system.log.info(s"delete: $count") })
 
   system.registerOnTermination { () => session.close() }
 }
@@ -35,14 +40,14 @@ case class UserRepository()(implicit val session: SlickSession){
 
   def findAll = Slick.source(
     sql"""
-          SELECT users.id, users.name, roles.id, roles.name
+          SELECT users.id, users.name, users.score, roles.id, roles.name
           FROM users LEFT JOIN roles
           ON users.role_id = roles.id
       """.as[User])
 
   def findById(id: Int) = Slick.source(
     sql"""
-          SELECT users.id, users.name, roles.id, roles.name
+          SELECT users.id, users.name, users.score, roles.id, roles.name
           FROM users LEFT JOIN roles
           ON users.role_id = roles.id
           WHERE users.id = '#$id'
@@ -50,7 +55,7 @@ case class UserRepository()(implicit val session: SlickSession){
 
   def findByName(name: String) = Slick.source(
     sql"""
-          SELECT users.id, users.name, roles.id, roles.name
+          SELECT users.id, users.name, users.score, roles.id, roles.name
           FROM users LEFT JOIN roles
           ON users.role_id = roles.id
           WHERE users.name = '#$name'
@@ -67,26 +72,26 @@ case class UserRepository()(implicit val session: SlickSession){
     .via(Slick.flow(newUser => newUser.role match {
       case Some(value) =>
         sqlu"""
-               INSERT INTO users(name, role_id)
-               VALUES ('#${newUser.name}', '#${value.id}')
+               INSERT INTO users(name, score, role_id)
+               VALUES ('#${newUser.name}', #${newUser.score}, #${value.id})
           """
       case _ =>
         sqlu"""
-               INSERT INTO users(name)
-               VALUES ('#${newUser.name}')
+               INSERT INTO users(name, score)
+               VALUES ('#${newUser.name}', '#${newUser.score}')
           """
     }))
 
-  def delete(user: User) = Source.single(user)
-    .via(Slick.flow(deleteUser => sqlu"""DELETE FROM users WHERE users.id = #${deleteUser.id}"""))
+  def deleteById(id: Int) = Source.single(id)
+    .via(Slick.flow(userId => sqlu"""DELETE FROM users WHERE users.id = #$id"""))
 }
 
-case class User(id: Int = 0, name: String, role: Option[Role] = None)
+case class User(id: Option[Int] = None, name: String, score: Double = 0.0, role: Option[Role] = None)
 
 object User {
 
   def result = GetResult { result =>
-    User(result.nextInt(), result.nextString(), result.nextIntOption()
+    User(result.nextIntOption(), result.nextString(), result.nextDouble(), result.nextIntOption()
       .map { Role(_, result.nextString()) })
   }
 }
